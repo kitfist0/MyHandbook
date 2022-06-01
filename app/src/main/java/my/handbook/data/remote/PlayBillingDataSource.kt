d3f07f58a0ba41418f8c0729adde348d1b6f451d
kitfist0 <kitfist0@yandex.ru>
2022-06-01 23:13:20 +0300

    Make launch billing flow suspend

--- app/src/main/java/my/handbook/data/remote/PlayBillingDataSource.kt
+++ app/src/main/java/my/handbook/data/remote/PlayBillingDataSource.kt
@@ -2,8 +2,10 @@ package my.handbook.data.remote
 
 import android.app.Activity
 import com.android.billingclient.api.*
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import my.handbook.BuildConfig
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.coroutines.resume
@@ -12,17 +14,53 @@ import kotlin.coroutines.suspendCoroutine
 
 @Singleton
 class PlayBillingDataSource @Inject constructor(
-    private val billingClient: BillingClient,
+    billingClientBuilder: BillingClient.Builder,
 ) {
 
-    private val billingConnectionMutex = Mutex()
-    private val productDetailsMap = mutableMapOf<String, ProductDetails>()
+    private val purchasesUpdatesChannel: Channel<PurchasedIdsResponse> = Channel(Channel.UNLIMITED)
 
-    suspend fun queryProductDetails(productIds: List<String>): ProductDetailsResult {
+    private val purchasesUpdatedListener = PurchasesUpdatedListener { billingResult, purchases ->
+        if (billingResult.isOk() && purchases?.isNotEmpty() == true) {
+            val purchasedIds = purchases
+                .filter { purchase -> purchase.purchaseState == Purchase.PurchaseState.PURCHASED }
+                .flatMap { purchase -> purchase.products }
+            purchasesUpdatesChannel.trySend(PurchasedIdsResponse.Success(purchasedIds))
+        } else {
+            purchasesUpdatesChannel.trySend(PurchasedIdsResponse.Error(billingResult.getErrorMessage()))
+        }
+    }
+
+    private val billingClient = billingClientBuilder
+        .setListener(purchasesUpdatedListener)
+        .enablePendingPurchases()
+        .build()
+
+    private val billingConnectionMutex = Mutex()
+
+    suspend fun purchaseProduct(activity: Activity, productId: String): PurchasedIdsResponse {
         billingClient.ensureReady()
-        val queryProductDetailsParams = QueryProductDetailsParams.newBuilder()
+        return queryProductDetailsList()
+            .find { it.productId == productId }
+            ?.let { productDetails ->
+                val billingFlowParams = BillingFlowParams.newBuilder()
+                    .setProductDetailsParamsList(
+                        listOf(
+                            BillingFlowParams.ProductDetailsParams.newBuilder()
+                                .setProductDetails(productDetails)
+                                .build()
+                        )
+                    )
+                    .build()
+                billingClient.launchBillingFlow(activity, billingFlowParams)
+                purchasesUpdatesChannel.receive()
+            }
+            ?: PurchasedIdsResponse.Error("Product id not found")
+    }
+
+    suspend fun queryProductDetailsList(): List<ProductDetails> {
+        val params = QueryProductDetailsParams.newBuilder()
             .setProductList(
-                productIds.map { productId ->
+                BuildConfig.PRODUCT_IDS.toList().map { productId ->
                     QueryProductDetailsParams.Product.newBuilder()
                         .setProductId(productId)
                         .setProductType(BillingClient.ProductType.INAPP)
@@ -30,38 +68,14 @@ class PlayBillingDataSource @Inject constructor(
                 }
             )
             .build()
-        val productDetailsResult = billingClient.queryProductDetails(queryProductDetailsParams)
-        productDetailsResult.productDetailsList?.let { list ->
-            list.forEach { details -> productDetailsMap[details.productId] = details }
-        }
-        return productDetailsResult
+        return billingClient.queryProductDetails(params).productDetailsList.orEmpty()
     }
 
-    suspend fun queryPurchases(): PurchasesResult {
-        billingClient.ensureReady()
+    suspend fun queryPurchasesList(): List<Purchase> {
         val queryPurchasesParams = QueryPurchasesParams.newBuilder()
             .setProductType(BillingClient.ProductType.INAPP)
             .build()
-        return billingClient.queryPurchasesAsync(queryPurchasesParams)
-    }
-
-    fun purchaseProduct(activity: Activity, productId: String): Boolean {
-        val productDetails = productDetailsMap[productId]
-        return if (billingClient.isReady && productDetails != null) {
-            val billingFlowParams = BillingFlowParams.newBuilder()
-                .setProductDetailsParamsList(
-                    listOf(
-                        BillingFlowParams.ProductDetailsParams.newBuilder()
-                            .setProductDetails(productDetails)
-                            .build()
-                    )
-                )
-                .build()
-            billingClient.launchBillingFlow(activity, billingFlowParams)
-            true
-        } else {
-            false
-        }
+        return billingClient.queryPurchasesAsync(queryPurchasesParams).purchasesList
     }
 
     /**
@@ -78,24 +92,38 @@ class PlayBillingDataSource @Inject constructor(
         connectOrThrow()
     }
 
-    private suspend fun BillingClient.connectOrThrow() = suspendCoroutine<Unit> { cont ->
-        startConnection(object : BillingClientStateListener {
-            override fun onBillingSetupFinished(result: BillingResult) {
-                if (result.isOk()) {
-                    cont.resume(Unit)
-                } else {
-                    cont.resumeWithException(
-                        RuntimeException("Billing conn failed: ${result.debugMessage}")
-                    )
+    private suspend fun BillingClient.connectOrThrow() = suspendCoroutine<Unit> { continuation ->
+        startConnection(
+            object : BillingClientStateListener {
+                override fun onBillingSetupFinished(result: BillingResult) {
+                    if (result.isOk()) {
+                        continuation.resume(Unit)
+                    } else {
+                        continuation.resumeWithException(
+                            RuntimeException("Billing conn failed: ${result.debugMessage}")
+                        )
+                    }
+                }
+
+                override fun onBillingServiceDisconnected() {
+                    // No need to setup reconnection logic here, call ensureReady()
+                    // before each purchase to reconnect as necessary
                 }
             }
-
-            override fun onBillingServiceDisconnected() {
-                // No need to setup reconnection logic here, call ensureReady()
-                // before each purchase to reconnect as necessary
-            }
-        })
+        )
     }
 
     private fun BillingResult.isOk() = responseCode == BillingClient.BillingResponseCode.OK
+
+    private fun BillingResult.getErrorMessage() = when (responseCode) {
+        BillingClient.BillingResponseCode.SERVICE_TIMEOUT -> "Service timeout"
+        BillingClient.BillingResponseCode.FEATURE_NOT_SUPPORTED -> "Feature not supported"
+        BillingClient.BillingResponseCode.SERVICE_DISCONNECTED -> "Service disconnected"
+        BillingClient.BillingResponseCode.SERVICE_UNAVAILABLE -> "Service unavailable"
+        BillingClient.BillingResponseCode.BILLING_UNAVAILABLE -> "Billing unavailable"
+        BillingClient.BillingResponseCode.ITEM_UNAVAILABLE -> "Item unavailable"
+        BillingClient.BillingResponseCode.DEVELOPER_ERROR -> "Developer error"
+        BillingClient.BillingResponseCode.ERROR -> "Error"
+        else -> "Unknown error"
+    }
 }
