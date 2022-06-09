6de02b675f275c91483140d78a5727c3969032a1
kitfist0 <kitfist0@yandex.ru>
2022-06-09 19:33:09 +0300

    Add more billing response codes to error messages

--- app/src/main/java/my/handbook/data/remote/PlayBillingDataSource.kt
+++ app/src/main/java/my/handbook/data/remote/PlayBillingDataSource.kt
@@ -168,11 +168,14 @@ class PlayBillingDataSource @Inject constructor(
         BillingClient.BillingResponseCode.SERVICE_TIMEOUT -> "Service timeout"
         BillingClient.BillingResponseCode.FEATURE_NOT_SUPPORTED -> "Feature not supported"
         BillingClient.BillingResponseCode.SERVICE_DISCONNECTED -> "Service disconnected"
+        BillingClient.BillingResponseCode.USER_CANCELED -> "User canceled"
         BillingClient.BillingResponseCode.SERVICE_UNAVAILABLE -> "Service unavailable"
         BillingClient.BillingResponseCode.BILLING_UNAVAILABLE -> "Billing unavailable"
         BillingClient.BillingResponseCode.ITEM_UNAVAILABLE -> "Item unavailable"
         BillingClient.BillingResponseCode.DEVELOPER_ERROR -> "Developer error"
         BillingClient.BillingResponseCode.ERROR -> "Error"
+        BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED -> "Item already owned"
+        BillingClient.BillingResponseCode.ITEM_NOT_OWNED -> "Item not owned"
         else -> UNKNOWN_ERROR
     }
 }
